a0aa024dab9d0d289cbb39453f10014603eb8b83
Gustavo <gussand@gmail.com>
2024-01-15 04:36:20 +0000

    create tester file and add starcoder

--- fast.py
+++ fast.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, Depends, Security
+from fastapi import FastAPI, HTTPException, Depends, Security, Header
 from pydantic import BaseModel
 from fastapi.security.api_key import APIKeyHeader, APIKey 
 from typing import Optional
@@ -10,6 +10,8 @@ import torch
 import time
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
+
+transfomers_token = "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"
 
 tokenizer = AutoTokenizer.from_pretrained("codellama/CodeLlama-7b-hf")
 
@@ -24,6 +26,10 @@ pipeline = transformers.pipeline(
 tokenizer_phi2 = AutoTokenizer.from_pretrained("microsoft/phi-2")
 model_phi2 = AutoModelForCausalLM.from_pretrained("microsoft/phi-2")
 
+starcoder =  "bigcode/starcoder"
+tokenizer_starcoder = AutoTokenizer.from_pretrained(starcoder)
+model_starcoder = AutoModelForCausalLM.from_pretrained(starcoder).to("cuda")
+
 #load environment variables 
 load_dotenv()
 app = FastAPI()
@@ -36,36 +42,25 @@ api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=False)
 class Prompt(BaseModel):
     text: str
 
-async def get_api_key( api_key_header: Optional[str] = Security(api_key_header)):
-    if api_key_header == API_KEY:
-        return api_key_header
-    else:
-        raise HTTPException(status_code=403, detail="Invalid API Key")
+
+def process_llama_7b(prompt_data):
+
+    sequences = pipeline(
+        prompt_data,
+        do_sample=True,
+        temperature=0.6,
+        top_p=0.9,
+        num_return_sequences=1,
+        eos_token_id=tokenizer.eos_token_id,
+        max_length=100,
+    )
+
+    return sequences[0]['generated_text']
     
-@app.post("/prompt_phi2/")
-async def prompt_phi2(prompt: Prompt, api_key: APIKey = Depends(get_api_key)):
 
-    try:
-        response = process_prompt_phi2(prompt.text)
-        return {"response":response}
-    
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+def process_phi2(prompt_data):
 
-
-@app.post("/prompt_llama7b/")
-async def prompt_llama7b(prompt: Prompt, api_key: APIKey = Depends(get_api_key)):
-
-    try:
-        response = process_prompt_llama7b(prompt.text)
-        return {"response":response}
-    
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-    
-def process_prompt_phi2(prompt):
-
-    inputs = tokenizer_phi2.encode(prompt, return_tensors="pt", return_attention_mask=False)
+    inputs = tokenizer_phi2.encode(prompt_data, return_tensors="pt", return_attention_mask=False)
     outputs = model_phi2.generate(inputs, 
                                   max_length=100, 
                                   do_sample=True, 
@@ -77,19 +72,67 @@ def process_prompt_phi2(prompt):
     return generated_text
 
 
-def process_prompt_llama7b(prompt):
+def process_starcoder(prompt_data):
 
-        sequences = pipeline(
-            prompt,
-            do_sample=True,
-            temperature=0.6,
-            top_p=0.9,
-            num_return_sequences=1,
-            eos_token_id=tokenizer.eos_token_id,
-            max_length=100,
-        )
+    input = tokenizer_starcoder.encode(prompt_data, return_tensors="pt", return_attention_mask=False).to("cuda")
+    outputs = model_starcoder.generate(input, 
+                                  max_length=100, 
+                                  do_sample=True, 
+                                  top_p=0.95, 
+                                  top_k=60, 
+                                  num_return_sequences=1)
+    generated_text = tokenizer_starcoder.decode(outputs[0], skip_special_tokens=True)
+    return generated_text
 
-        return sequences[0]['generated_text']
+
+model_functions = {
+    "llama_7b": process_llama_7b,
+    "phi2": process_phi2,
+    "starcoder": process_starcoder,
+}
+
+@app.post("/process_prompt/")
+async def process_prompt(prompt_data: dict, authorization: str = Header(None)):
+
+    try:
+        # Check the authorization header for the access token
+        expected_api_key = "BAD_F00D"
+        if authorization != f"Bearer {expected_api_key}":
+            raise HTTPException(status_code=401, detail="Unauthorized")
+        
+        if "model" not in prompt_data:
+            raise HTTPException(status_code=400, detail="Missing model parameter")
+
+        # Extract data from the JSON request
+        model = prompt_data.get("model")
+
+        if model not in model_functions:
+            raise HTTPException(status_code=400, detail="Invalid model parameter")
+        
+        prompt = prompt_data.get("prompt")
+        
+        if prompt is None:
+            raise HTTPException(status_code=400, detail="Missing prompt parameter")
+        
+        max_tokens = prompt_data.get("max_tokens", 100)
+
+
+        # Call the appropriate Python function based on the model
+        the_response = model_functions[model](prompt)
+
+        # Perform processing using the received data (replace this with your actual logic)
+        result = {
+            "model": model,
+            "prompt": prompt,
+            "max_tokens": max_tokens,
+            "response": the_response # Replace with your actual response
+        }
+
+        return result
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
  
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
---
+++ tester.py
@@ -0,0 +1,63 @@
+import requests
+import json
+import torch
+import time
+
+# Set your OpenAI API key
+api_key = "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"
+api_llama = "BAD_F00D"
+
+# Set the API endpoint URL for OpenAI GPT-3.5-turbo
+url = "https://api.openai.com/v1/completions"
+url_llama = "http://localhost:8000/process_prompt/"
+
+# Prepare headers
+headers = {
+    "Content-Type": "application/json",
+    "Authorization": f"Bearer {api_llama}"
+}
+
+# Prepare the data to be sent in the POST request
+# This is where you set your prompt and other parameters
+#    "prompt": "The code for a basic Python function that adds two numbers is:", # Your prompt here
+
+prompt = "# Print all primes between 1 and n \n def print_primes(n):"
+data = {
+
+    #"model": "gpt-3.5-turbo-instruct",  # Codex model for code-related tasks
+    #"model": "phi2",  # Codex model for code-related tasks
+    "model": "llama_7b",  # Codex model for code-related tasks
+    #"model": "starcoder",  # Codex model for code-related tasks
+    "prompt": prompt, 
+    "max_tokens": 100, 
+}
+
+# if torch.cuda.is_available():
+# #   torch.set_default_tensor_type(torch.cuda.HalfTensor)
+#     print("cuda!!")
+# elif torch.backends.mps.is_available():
+#     mps_device = torch.device("mps")
+#     x = torch.ones(1, device=mps_device)
+#     print(x)
+# else:
+#     print("Nope")
+
+st = time.time()
+
+# Send the POST request
+response = requests.post(url_llama, headers=headers, json=data)
+
+# Check if the request was successful
+if response.status_code == 200:
+    # Parse and print the response
+    print(json.dumps(response.json(), indent=4))
+else:
+    # Print error message
+    print("Error:", response.status_code, response.text)
+
+#get the end time
+et = time.time()
+
+# get the execution time
+elapsed_time = et - st
+print('Execution time:', elapsed_time, 'seconds')
