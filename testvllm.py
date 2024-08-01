de008cebf0f0c131cacb212f3f56083d17158f90
Gus Sandoval <gussand@gmail.com>
2024-08-01 14:10:04 -0400

    get up to date

--- testvllm.py
+++ testvllm.py
@@ -1,4 +1,5 @@
 from openai import OpenAI
+import time 
 
 # Modify OpenAI's API key and API base to use vLLM's API server.
 openai_api_key = "EMPTY"
@@ -8,11 +9,24 @@ client = OpenAI(
     base_url=openai_api_base,
 )
 
+# Send a request to the vLLM API server.
+# The prompt is a Python function that prints all prime numbers between 1 and n.
+# The model is vLLM's Starcoder model.
+
+# time how long it takes to get a response
+st = time.time()
+
 completion = client.completions.create(model="bigcode/starcoder",
                                       prompt="# Print all primes between 1 and n \n def print_primes(n):")
 
 print('Starcoder: \n')
-print("Completion result:", completion.choices[0].text)
+print("Completion result:", completion)
+
+for choice in completion.choices:
+    print("Completion result:", choice.text)
+
+t = time.time() - st
+print("Time:", t)
 
 # completion = client.completions.create(model="meta-llama/Llama-2-13b-hf",
 #                                       prompt="# Print all primes between 1 and n \n def print_primes(n):")
